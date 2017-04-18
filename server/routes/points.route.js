b473e3b767955cafe8e83a984eca04bb7ecc6a66
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-04-18 11:41:29 -0600

    add endpoint add points

--- server/routes/points.route.js
+++ server/routes/points.route.js
@@ -6,13 +6,13 @@ const router = express.Router(); // eslint-disable-line new-cap
 /**
  * POST  - increments one point to desired user
  */
-router.route('/points/add')
+router.route('/add')
   .post(pointsCtrl.addPoints);
 
 /**
  * GET - obtains points for desired user
  */
-router.route('/points/get/:userId')
+router.route('/get/:userId')
   .get(pointsCtrl.getPoints);
 
 
